edeaa872632688b6dbbb5564dcb76b70c03d49b7
ysmy12345 <shumyongsheng@gmail.com>
2026-04-20 14:23:59 +0800

    test

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello! testing</h1>");
+});
+
+app.listen(5000, () => {
+  console.log("✅ start running：http://localhost:5000");
+});
